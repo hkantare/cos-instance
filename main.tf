a37bb1f389bc2b275f7f54363b6f84eef32fe3ce
HARINI KANTAREDDY <hkantare@in.ibm.com>
2023-01-20 16:42:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "1.49.0"
+    }
+  }
+}
+
 provider "ibm" {
   ibmcloud_api_key      = "${var.ibmcloud_api_key}"
   //iaas_classic_username = "${var.iaas_classic_username}"
